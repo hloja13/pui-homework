00a7c3bde97695bf07a3f0bddcb29a2fb98bec01
hloja13 <143621913+hloja13@users.noreply.github.com>
2023-09-25 22:47:57 -0400

    Hw3

--- solution-hw3/price_option.js
+++ solution-hw3/price_option.js
@@ -1,23 +1,20 @@
-// Glazing Options
+
 const glazingOptions = [
     { name: 'Keep original', priceAdaptation: 0.00 },
     { name: 'Sugar milk', priceAdaptation: 0.00 },
     { name: 'Vanilla milk', priceAdaptation: 0.50 },
     { name: 'Double chocolate', priceAdaptation: 1.50 },
-];
-  
-  // Pack Size Options
+];  
+
 const packSizeOptions = [
     { name: '1', quantity: 1 },
     { name: '3', quantity: 3 },
     { name: '6', quantity: 6 },
     { name: '12', quantity: 12 },
-];
-  
-  // Base price for a cinnamon roll
+];  
+
 const basePrice = 2.49;
 
-// Populate Glazing Options dropdown
 const glazingSelect = document.getElementById('glazingOptions');
 for (const option of glazingOptions) {
   const optionElement = document.createElement('option');
@@ -26,11 +23,10 @@ for (const option of glazingOptions) {
   glazingSelect.appendChild(optionElement);
 }
 
-// Populate Pack Size Options dropdown
 const packSizeSelect = document.getElementById('packSizeOptions');
 for (const option of packSizeOptions) {
   const optionElement = document.createElement('option');
-  optionElement.value = option.quantity; // Set the quantity as the option value
+  optionElement.value = option.quantity;
   optionElement.textContent = option.name;
   packSizeSelect.appendChild(optionElement);
 }
@@ -45,6 +41,5 @@ function updatePrice() {
   
     const totalPrice = (basePrice + selectedGlazingPrice) * selectedPackSizeQuantity;
   
-    // Update the displayed total price
     totalPriceSpan.textContent = `$ ${totalPrice.toFixed(2)}`;
 }
